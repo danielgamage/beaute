33762cbee9b8b138cf7af14f5797dabca28c2fcb
Daniel Gamage <hello@danielgamage.com>
2014-02-07 01:18:31 -0500

    smooth as butter

--- js/main.js
+++ js/main.js
@@ -1,34 +1,28 @@
 $(document).ready(function() {
-	  // Handler for .ready() called.
-	// $( "body" ).mouseenter(function( event ) {
-	// 	var mX = event.pageX;
-	// 		mY = event.pageY;
-	// 	$( "h1" ).animate({marginLeft: (mX/-3.4)});
-	// 	$( "h2" ).animate({marginLeft: (mX/-2.1)});
-	// 	$( "h3" ).animate({marginLeft: (mX/-1.1)});
-	// });
 	var source      = $("#quote-template");
 	var source_data = source.html();
 	var template    = Handlebars.compile(source_data);
 
 	$.getJSON( "quotes.json", function( data ) {
 		source.parent().html(template(data));
+		$(".quote").addClass("default");
+		$(".quote").children('[lang="en"]').hide();
 		$(".quote").click(function(){
-			$(this).toggleClass("translated");
+			$(this).toggleClass("translated").children('[lang="en"]').toggle(100);
 		});
+	});
 
-		$( "header" ).mousemove(function( event ) {
-			var mX = event.pageX;
-				mY = event.pageY;
-			$( "h1" ).css({
-				"margin-left": (mX/-2.4)
-			});
-			$( "h2" ).css({
-				"margin-left": (mX/-1.8)
-			});
-			$( "h3" ).css({
-				"margin-left": (mX/-1.2)
-			});
+	$( "header" ).mousemove(function( event ) {
+		var mX = event.pageX;
+			mY = event.pageY;
+		$( "h1" ).css({
+			"margin-left": (mX/-2.4)
+		});
+		$( "h2" ).css({
+			"margin-left": (mX/-1.8)
+		});
+		$( "h3" ).css({
+			"margin-left": (mX/-1.2)
 		});
 	});
 });
