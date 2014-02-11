a3d0c675b541bb780eefa495afedfe0f3d3afb9d
Daniel Gamage <hello@danielgamage.com>
2014-02-11 08:35:23 -0500

    touch ups

--- js/main.js
+++ js/main.js
@@ -3,16 +3,22 @@ $(document).ready(function() {
 	var source_data = source.html();
 	var template    = Handlebars.compile(source_data);
 
-	$.getJSON( "quotes.json", function( data ) {
-		source.parent().html(template(data));
-		$(".quote").addClass("default");
-		$(".quote").children('[lang="en"]').hide();
-		$(".quote").click(function(){
+	var quoteToggle = function() {
+		var quote = $(".quote");
+
+		quote.addClass("default");
+		quote.children('[lang="en"]').hide();
+		quote.click(function(){
 			$(this).toggleClass("translated").children('[lang="en"]').toggle(100);
 		});
+	}
+
+	$.getJSON( "quotes.json", function( data ) {
+		source.parent().html(template(data));
+		quoteToggle(); // once the quotes are loaded
 	});
 
-	$( "header" ).mousemove(function( event ) {
+	$( "header" ).click(function( event ) {
 		var mX = event.pageX;
 			mY = event.pageY;
 		$( "h1" ).css({
